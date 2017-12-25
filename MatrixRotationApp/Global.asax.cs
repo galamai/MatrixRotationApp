d04a8e8f6324940e73fc428ad8d0f53411093595
Ievgen Galamai <galamayevg@gmail.com>
2017-12-25 16:17:15 +0200

    Добавил создание директории /App_Data при старте, если не существует.

--- MatrixRotationApp/Global.asax.cs
+++ MatrixRotationApp/Global.asax.cs
@@ -3,6 +3,7 @@ using Autofac.Integration.Mvc;
 using MatrixRotationApp.Services;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -30,6 +31,8 @@ namespace MatrixRotationApp
 
             var container = builder.Build();
             DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
+            Directory.CreateDirectory(Server.MapPath("~/App_Data"));
         }
     }
 }
